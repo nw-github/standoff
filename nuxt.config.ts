cc43f491175f6259290e32a94e4a331dbb16125d
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2025-02-08 01:23:51 -0500

    upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    plugins: ["~/plugins/socket.ts"]
+    plugins: ["~/plugins/socket.ts"],
+    compatibilityDate: "2025-02-08",
+    nitro: {
+        experimental: {
+            websocket: true,
+        },
+    },
 });
